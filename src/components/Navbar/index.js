b862e9bdad1459f04e09f269338f333cc826daa6
Watts-James <101371479+Watts-James@users.noreply.github.com>
2022-07-06 19:54:35 -0500

    Personal Website work in progress

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav,NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>James Watts</NavLogo>
+                <MobileIcon onClick={toggle}>
+                  <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                  <NavItem>
+                    <NavLinks to='home'>Home</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='projects'>Projects</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='about'>About</NavLinks>
+                  </NavItem>
+                </NavMenu>
+                <NavBtn>
+                  <NavBtnLink to="/link">Link</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+    </>
+  );
+};
+
+export default Navbar
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap,SidebarRoute } from './SidebarElements';
+
+const Sidebar = ({isOpen,toggle}) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+        <Icon onClick={toggle}>
+            <CloseIcon />
+        </Icon>
+        <SidebarWrapper>
+            <SidebarMenu>
+                <SidebarLink to="home" onClick={toggle}>
+                    Home
+                </SidebarLink>
+                <SidebarLink to="projects" onClick={toggle}>
+                    Projects
+                </SidebarLink>
+                <SidebarLink to="about" onClick={toggle}>
+                    About
+                </SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to="/link">Link</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
