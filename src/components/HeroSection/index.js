f83d333f0134acb5606344cc6bb88dd608ddd5d4
Watts-James <101371479+Watts-James@users.noreply.github.com>
2022-07-08 12:56:19 -0500

    Added splash text to hero page

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements'
+import {Button} from '../ButtonElement';
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
 
 const HeroSection = () => {
+  const [hover,setHover] = useState(false)
+  const onHover = () => {
+    setHover(!hover);
+  }
   return (
     <HeroContainer>
         <HeroBg>
-            <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+            <VideoBg autoPlay loop muted src={Video} type='Video/mp4'/>
         </HeroBg>
+        <HeroContent>
+          <HeroH1> James Watts</HeroH1>
+          <HeroP>
+            Full Stack developer, all around cool guy.
+          </HeroP>
+          <HeroBtnWrapper>
+            <Button to='/Link' onMouseEnter={onHover} onMouseLeave={onHover}>
+              Résumé {hover ? <ArrowForward /> : <ArrowRight/>}
+            </Button>
+          </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
